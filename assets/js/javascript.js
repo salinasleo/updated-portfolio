094ec73a6f05837063cbdd874151fd144a6960f8
salinasleo <salinasleo@gmail.com>
2018-10-09 22:05:56 -0400

    updated portfolio

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+function hoverpic(profile_pic) {
+    profile_pic.setAttribute('src', 'assets/images/hover.jpg');
+  }
+
+  function unhover(profile_pic) {
+    profile_pic.setAttribute('src', 'assets/images/default.jpg');
+  }
+
+  function clickpic(profile_pic) {
+    profile_pic.setAttribute('src', 'assets/images/click.jpg');
+  }
